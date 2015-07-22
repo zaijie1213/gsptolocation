3ca468f41094d6c5ced861ebeaa4ea8a57f575c3
zaijie <zaijie8@gmail.com>
2015-07-22 09:13:07 +0800

    add butterknife

--- app/src/main/java/com/example/zaijie/gpslocation/MainActivity.java
+++ app/src/main/java/com/example/zaijie/gpslocation/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.zaijie.gpslocation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.lat)
+    EditText mLat;
+    @Bind(R.id.lng)
+    EditText mLng;
+    @Bind(R.id.btn_search)
+    Button mBtnSearch;
+    @Bind(R.id.result)
+    TextView mResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
 

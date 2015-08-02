caf3dad97e91e7d7ba0391c5585dca01a3a9b7cd
zaijie <zaijie@zaijiedeMacBook-Air.local>
2015-08-02 21:33:20 +0800

    init

--- app/src/main/java/com/example/zaijie/gpslocation/MainActivity.java
+++ app/src/main/java/com/example/zaijie/gpslocation/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.zaijie.gpslocation;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +15,8 @@ import android.widget.TextView;
 import com.thinkland.sdk.android.DataCallBack;
 import com.thinkland.sdk.android.JuheData;
 import com.thinkland.sdk.android.Parameters;
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.update.UmengUpdateAgent;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -35,10 +41,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        UmengUpdateAgent.update(this);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
     }
 
     @OnClick(R.id.btn_search)
     public void Search() {
+        mBtnSearch.setEnabled(false);
+        mResult.setText("`");
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        final ProgressDialog dialog = ProgressDialog.show(this, "查询中", "正在请求数据", false, false);
+        dialog.show();
+
         String lat = mLat.getText().toString();
         String lng = mLng.getText().toString();
         if ((!TextUtils.isEmpty(lat)) && (!TextUtils.isEmpty(lng))) {
@@ -50,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(int i, String s) {
                     try {
+                        mBtnSearch.setEnabled(true);
+                        dialog.dismiss();
                         Log.d("szg", s);
                         JSONObject object = new JSONObject(s);
                         String addr = object.getJSONObject("result").getString("address");
@@ -65,12 +98,16 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onFinish() {
+                    dialog.dismiss();
+                    mBtnSearch.setEnabled(true);
 
                 }
 
                 @Override
                 public void onFailure(int i, String s, Throwable throwable) {
                     mResult.setText(R.string.err);
+                    mBtnSearch.setEnabled(true);
+                    dialog.dismiss();
                 }
             });
         }

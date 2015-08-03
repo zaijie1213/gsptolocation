f072516778cb7173211d46b39a0ddac6848c4bb9
zaijie <zaijie@zaijiedeMacBook-Air.local>
2015-08-03 22:34:06 +0800

    添加定位功能

--- app/src/main/java/com/example/zaijie/gpslocation/MainActivity.java
+++ app/src/main/java/com/example/zaijie/gpslocation/MainActivity.java
@@ -12,6 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.thinkland.sdk.android.DataCallBack;
 import com.thinkland.sdk.android.JuheData;
 import com.thinkland.sdk.android.Parameters;
@@ -27,6 +31,8 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    public BDLocationListener listener;
+    public LocationClient client;
     @Bind(R.id.lat)
     EditText mLat;
     @Bind(R.id.lng)
@@ -35,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     Button mBtnSearch;
     @Bind(R.id.result)
     TextView mResult;
+    @Bind(R.id.getCurLoc)
+    Button mGetCurLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         UmengUpdateAgent.update(this);
+        initLocation();
+    }
+
+    private void initLocation() {
+        client = MyApp.getmLocationClient();
+        client.registerLocationListener(new BDLocationListener() {
+            @Override
+            public void onReceiveLocation(BDLocation bdLocation) {
+                String lati = String.valueOf(bdLocation.getLatitude());
+                String langti = String.valueOf(bdLocation.getLongitude());
+                mLng.setText(langti);
+                mLat.setText(lati);
+            }
+        });
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+//        int span = 5000;
+//        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+//        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认false，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        client.setLocOption(option);
 
     }
 
@@ -110,6 +147,13 @@ public class MainActivity extends AppCompatActivity {
                     dialog.dismiss();
                 }
             });
+        }
+    }
+
+    @OnClick(R.id.getCurLoc)
+    public void getCurLoc() {
+        if (!client.isStarted()) {
+            client.start();
         }
     }
 }
--- app/src/main/java/com/example/zaijie/gpslocation/MyApp.java
+++ app/src/main/java/com/example/zaijie/gpslocation/MyApp.java
@@ -2,15 +2,26 @@ package com.example.zaijie.gpslocation;
 
 import android.app.Application;
 
+import com.baidu.location.LocationClient;
 import com.thinkland.sdk.android.JuheSDKInitializer;
 
 /**
  * Created by zaijie on 15/7/22.
  */
 public class MyApp extends Application {
+
+    public static LocationClient mLocationClient = null;
+
+    public static LocationClient getmLocationClient() {
+        return mLocationClient;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         JuheSDKInitializer.initialize(this);
+        mLocationClient = new LocationClient(this);
     }
+
+
 }
